4d6aced84cb98cc4b2a9280a77ba55b1aa107d63
Kenny Do <kedo@patreon.com>
2017-08-06 14:31:53 -0700

    Basic slack connectivity

--- papikafromslack/src/main/kotlin/net/hanekawa/papika/fromslack/Config.kt
+++ papikafromslack/src/main/kotlin/net/hanekawa/papika/fromslack/Config.kt
@@ -3,13 +3,14 @@ package net.hanekawa.papika.fromslack
 import com.typesafe.config.ConfigFactory
 import io.github.config4k.extract
 
-data class Config(val kafkaBootstrapServers: String, val kafkaTopic: String) {
+data class Config(val kafkaBootstrapServers: String, val kafkaTopic: String, val slackApiToken: String) {
     companion object Factory {
         fun load(): Config {
             val config = ConfigFactory.load()
             return Config(
                     kafkaBootstrapServers = config.extract<String>("kafkaBootstrapServers"),
-                    kafkaTopic = config.extract<String>("kafkaTopic")
+                    kafkaTopic = config.extract<String>("kafkaTopic"),
+                    slackApiToken = config.extract<String>("slackApiToken")
             )
         }
     }
--- papikafromslack/src/main/kotlin/net/hanekawa/papika/fromslack/PapikaFromSlack.kt
+++ papikafromslack/src/main/kotlin/net/hanekawa/papika/fromslack/PapikaFromSlack.kt
@@ -1,22 +1,40 @@
 package net.hanekawa.papika.fromslack
 
+import com.ullink.slack.simpleslackapi.SlackSession
+import com.ullink.slack.simpleslackapi.events.UserTyping
+import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory
+import com.ullink.slack.simpleslackapi.listeners.UserTypingListener
 import net.hanekawa.papika.common.logging.getLogger
+import java.util.concurrent.TimeUnit
 
-class PapikaFromSlackBridge {
+import javax.websocket.Endpoint
+
+
+class Foo : UserTypingListener {
+    override fun onEvent(event: UserTyping?, session: SlackSession?) {
+        println(event.toString())
+    }
+
+}
+
+class PapikaFromSlackBridge(val config: Config) {
     companion object {
         val LOG = getLogger(this::class.java)
     }
 
     fun run() {
-        val config = Config.load()
-
         LOG.info("Hello world, I'm in the from-slack bridge")
         LOG.info("Config: {}", config.toString())
+
+        val slackSession = SlackSessionFactory.createWebSocketSlackSession(config.slackApiToken)
+        slackSession.addUserTypingListener(Foo())
+        slackSession.connect()
     }
 }
 
 
 fun main(args: Array<String>) {
-    val bridge = PapikaFromSlackBridge()
+    val config = Config.load()
+    val bridge = PapikaFromSlackBridge(config)
     bridge.run()
 }
